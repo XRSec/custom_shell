d752686f6199025d69f86a0e82362c5578122047
星冉 <Jalapeno1868@outlook.com>
2023-08-15 00:20:41 +0800

    Update 删除空文件夹.sh

--- 删除空文件夹.sh
+++ 删除空文件夹.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-# 目标文件夹
 source_folder="/volume1/影院/"
-# 排除文件夹
 exclude_folder="/volume1/影院/#recycle"
-# 移动到垃圾桶
 dest_folder="/volume1/影院/#recycle"
 
-find "$source_folder" -type d -empty| grep -v ${exclude_folder} | while read empty_dir; do
-    current_timestamp=$(date +%Y%m%d%H%M%S) # 获取时间戳
-    new_dir_name="$(basename "$empty_dir")_${current_timestamp}" # 获取文件名 设置新名字
-    printf "将[%s] 移动到 [%s/%s]" "${empty_dir}" "${dest_folder}" "${new_dir_name}" # 打印日志
-    mv "${empty_dir}" "${dest_folder}/${new_dir_name}"  # 移动文件
+find "$source_folder" -type d -empty| grep -v ${exclude_folder} | while read dir; do
+    if ! [ -L "$dir" ]; then # 跳过软链接
+        dir_size=$(du -s "$dir" | awk '{print $1}') # 获取目录大小
+        if [ "$dir_size" -lt 10 ]; then # 小于10KB的目录
+            current_timestamp=$(date +%Y%m%d%H%M%S) # 当前时间戳
+            new_dir_name="$(basename "$dir")_${current_timestamp}" # 新文件名
+            printf "将[%s] 移动到 [%s/%s]\n" "${dir}" "${dest_folder}" "${new_dir_name}" # 打印日志
+            mv "$dir" "${dest_folder}/${new_dir_name}" # 移动目录
+        fi
+    fi
 done
